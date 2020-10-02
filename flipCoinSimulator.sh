237d905b5dcf169c7fd664bbd13d708cf87f7a52
Manojgour05 <manojgour6811@gmail.com>
2020-10-02 22:47:39 +0530

    Refactor:Fliping a coin and count head & tail

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -10,3 +10,22 @@ then
 else
 	echo "Tail winner"
 fi
+
+#As a simulator, loop through Multiple times of flipping a coin
+
+hcount=0
+tcount=0
+for((i=0;i<=10;i++))
+do
+	num=$(($RANDOM%2))
+if [ $num -eq 0 ]
+	then
+		echo "Head"
+     		((hcount++))
+else
+		echo "Tail"
+		((tcount++))
+fi
+done
+		echo "Head won : $hcount"
+		echo "Tail won : $tcount"
